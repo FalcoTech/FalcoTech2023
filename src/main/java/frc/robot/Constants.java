07777648240abd85224c59d155f11daba33b610c
Gavin Precurato <gprecurato@gmail.com>
2023-01-08 13:14:29 -0500

    define pilot and copilot controller port numbers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,6 +14,7 @@ package frc.robot;
  */
 public final class Constants {
   public static class OperatorConstants {
+    // Creates port numbers for controllers
     public static final int PilotControllerPort = 0;
     public static final int CoPilotControllerPort = 1;
   }
