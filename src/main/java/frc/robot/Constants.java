a51b5aa6a9eb37ac815f4fc69739c365d9489055
Gavin Precurato <gprecurato@gmail.com>
2023-01-08 13:06:27 -0500

    add pilot and copilot controller port numbers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,6 +14,7 @@ package frc.robot;
  */
 public final class Constants {
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int PilotControllerPort = 0;
+    public static final int CoPilotControllerPort = 1;
   }
 }
