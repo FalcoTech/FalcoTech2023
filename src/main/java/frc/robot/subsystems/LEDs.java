c3cf59477f48308d083c86dfdef0bb6d5ac67497
Falco Tech <andy@yantes.com>
2023-01-30 20:54:48 -0500

    LEDs sub

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -7,8 +7,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDs extends SubsystemBase {
+  
   /** Creates a new LEDs. */
-  public LEDs() {}
+  public LEDs() {
+
+  }
 
   @Override
   public void periodic() {
