87b5b2a33e00871fa79a978b580644b539dafb54
Gavin Precurato <gprecurato@gmail.com>
2023-02-05 01:14:25 -0500

    drive train encoder stuff (again lol I'm losing it hahahaha)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,44 +6,41 @@ package frc.robot;
 
 import java.util.HashMap;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
-  public static class OperatorConstants { //Constants for OI/Driver controls 
-    //Controller Ports
+  public static class OperatorConstants { 
     public static final int PILOTCONTROLLERPORT = 0;
     public static final int COPILOTCONTROLLERPORT = 1;
   }
   
-  public static class DriveTrainConstants { //Constants for drive train stuff like motors/pneumatics
-    //Motor ID's
+  public static class DriveTrainConstants { 
     public static final int LEFTFRONTMOTOR_ID = 41;
     public static final int LEFTBACKMOTOR_ID = 43;
     public static final int RIGHTFRONTMOTOR_ID = 40;
     public static final int RIGHTBACKMOTOR_ID = 42;
-    //Encoder Values
-    public static final int DRIVETICKSPERREVOLUTION = 42;
-    public static final double DRIVEGEARRATIO_LOW = 16.36;
-    public static final double DRIVEGEARRATIO_HIGH = 7.95; 
-    //rpm DIVIDED BY gear ratio = free speed
-
-    public static final double DRIVEDISTPERREVOLUTION = 2 * Math.PI * .0762; //in meters
-    //6 INCH WHEELS THIS YEAR
-    public static final double DRIVEWHEELCIRCUMFERENCE = 2 * Math.PI * (4/2); /*radius in*/
     
-    //Shifter solenoid ID's
+    //DRIVE Encoder Values
+    public static final int ENCODERFULLREV = 4096; //TBD
+    public static final double GEARRATIO_LOW = 16.36;
+    public static final double GEARRATIO_HIGH = 7.95; 
+
+    public static final double WHEELRADIUSINCHES = 3; //6 inch wheels this year. /2 for radius
+    public static final double WHEELCIRCUMFERENCEINCHES = 2 * Math.PI * WHEELRADIUSINCHES;
+    public static final double TRACKWIDTH = Units.inchesToMeters(22); //TBD "Track width is the empirical measurement of the length from one center wheel to the opposite rail center wheel" - Google lol
+    public static final DifferentialDriveKinematics DRIVEKINEMATICS = new DifferentialDriveKinematics(TRACKWIDTH); 
+
+
+    //rpm DIVIDED BY gear ratio = free speed
+    
+    
+
     public static final int SHIFTSOLFORWARD_ID = 0;
     public static final int SHIFTSOLREVERSE_ID = 1;
   }
-  //Arm constants
+
   public static class ArmConstants{
     public static final int LEFTARMMOTOR_ID = 23; //change when we find out
     public static final int RIGHTARMMOTOR_ID = 24; //CHANGE WHEN FIND OUT caps lol
@@ -64,13 +61,14 @@ public final class Constants {
   public static class VisionConstants{
     public static final String TAGFAMILY = "tag16h5";
   }
+
   public static class LEDsConstants{
     public static final int LEDSTRIPLEFTPORT = 8;
     public static final int LEDSTRIPRIGHTPORT = 9;
     public static final int LEDSTRIPLENGTH = 180; //maybe 90 each, maybe 180 each? idk lol
   }
 
-  public static class PathPlannerConstants {
+  public static class AutoConstants {
     public static final HashMap<String, Command> AUTOEVENTMAP = new HashMap<>();
   }
   
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.Compressor;
@@ -28,16 +29,16 @@ import frc.robot.Constants.DriveTrainConstants;
 
 public class DriveTrain extends SubsystemBase {
   //Sparkmax motor controllers (old)
-  private final CANSparkMax leftFrontMotor = new CANSparkMax(DriveTrainConstants.LEFTFRONTMOTOR_ID, MotorType.kBrushless);
-  private final CANSparkMax leftBackMotor = new CANSparkMax(DriveTrainConstants.LEFTBACKMOTOR_ID, MotorType.kBrushless); 
-  private final CANSparkMax rightFrontMotor = new CANSparkMax(DriveTrainConstants.RIGHTFRONTMOTOR_ID, MotorType.kBrushless); 
-  private final CANSparkMax rightBackMotor = new CANSparkMax(DriveTrainConstants.RIGHTBACKMOTOR_ID, MotorType.kBrushless); 
+  // private final CANSparkMax leftFrontMotor = new CANSparkMax(DriveTrainConstants.LEFTFRONTMOTOR_ID, MotorType.kBrushless);
+  // private final CANSparkMax leftBackMotor = new CANSparkMax(DriveTrainConstants.LEFTBACKMOTOR_ID, MotorType.kBrushless); 
+  // private final CANSparkMax rightFrontMotor = new CANSparkMax(DriveTrainConstants.RIGHTFRONTMOTOR_ID, MotorType.kBrushless); 
+  // private final CANSparkMax rightBackMotor = new CANSparkMax(DriveTrainConstants.RIGHTBACKMOTOR_ID, MotorType.kBrushless); 
 
   //Falcon motor controllers
-  // private final WPI_TalonFX leftFrontMotor = new WPI_TalonFX(DriveTrainConstants.LEFTFRONTMOTOR_ID);
-  // private final WPI_TalonFX leftBackMotor = new WPI_TalonFX(DriveTrainConstants.LEFTBACKMOTOR_ID);
-  // private final WPI_TalonFX rightFrontMotor = new WPI_TalonFX(DriveTrainConstants.RIGHTFRONTMOTOR_ID);
-  // private final WPI_TalonFX rightBackMotor = new WPI_TalonFX(DriveTrainConstants.RIGHTBACKMOTOR_ID);
+  private final WPI_TalonFX leftFrontMotor = new WPI_TalonFX(DriveTrainConstants.LEFTFRONTMOTOR_ID);
+  private final WPI_TalonFX leftBackMotor = new WPI_TalonFX(DriveTrainConstants.LEFTBACKMOTOR_ID);
+  private final WPI_TalonFX rightFrontMotor = new WPI_TalonFX(DriveTrainConstants.RIGHTFRONTMOTOR_ID);
+  private final WPI_TalonFX rightBackMotor = new WPI_TalonFX(DriveTrainConstants.RIGHTBACKMOTOR_ID);
 
   //Differentialdrive groups
   private final MotorControllerGroup m_leftDriveGroup = new MotorControllerGroup(leftFrontMotor, leftBackMotor);
@@ -45,7 +46,7 @@ public class DriveTrain extends SubsystemBase {
   private final DifferentialDrive m_drive = new DifferentialDrive(m_leftDriveGroup, m_rightDriveGroup);
 
   //Odometry
-  // private final DifferentialDriveOdometry m_odometry; 
+  private final DifferentialDriveOdometry m_odometry; 
 
   //Gyro
   private final ADIS16470_IMU gyro = new ADIS16470_IMU();
@@ -69,24 +70,15 @@ public class DriveTrain extends SubsystemBase {
 
     coastDriveMotors(); //Start coasting drive motors
   
-    // leftFrontMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
-    // rightFrontMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
-    // leftFrontMotor.setSelectedSensorPosition(0);
-    // rightFrontMotor.setSelectedSensorPosition(0);
-    
-    //Odometry
-    // m_odometry = new DifferentialDriveOdometry(
-    //   getRotation2d(),
-    //   encoderTicksToMeters(leftFrontMotor.getSelectedSensorPosition(), 4096, DriveTrainConstants.DRIVEGEARRATIO_LOW, DriveTrainConstants.DRIVEWHEELCIRCUMFERENCE),
-    //   encoderTicksToMeters(rightFrontMotor.getSelectedSensorPosition(), 4096, DriveTrainConstants.DRIVEGEARRATIO_LOW, DriveTrainConstants.DRIVEWHEELCIRCUMFERENCE)
-    // );
+    leftFrontMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    rightFrontMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    leftFrontMotor.setSelectedSensorPosition(0);
+    rightFrontMotor.setSelectedSensorPosition(0);
 
-    // m_odometry.resetPosition(
-    //   getRotation2d(), 
-    //   encoderTicksToMeters(leftFrontMotor.getSelectedSensorPosition(), 4096, DriveTrainConstants.DRIVEGEARRATIO_LOW, DriveTrainConstants.DRIVEWHEELCIRCUMFERENCE), 
-    //   encoderTicksToMeters(rightFrontMotor.getSelectedSensorPosition(), 4096, DriveTrainConstants.DRIVEGEARRATIO_LOW, DriveTrainConstants.DRIVEWHEELCIRCUMFERENCE), 
-    //   new Pose2d()
-    // );
+    m_odometry = new DifferentialDriveOdometry(
+    getRotation2d(), 
+    encoderTicksToMeters(leftFrontMotor.getSelectedSensorPosition()), 
+    encoderTicksToMeters(rightFrontMotor.getSelectedSensorPosition()));
   }
 
   public void arcadeDrive(double speed, double rotation){   //Our main ArcadeDrive command. 
@@ -104,16 +96,16 @@ public class DriveTrain extends SubsystemBase {
   }
 
   public void brakeDriveMotors(){
-    leftFrontMotor.setIdleMode(IdleMode.kBrake);
-    leftBackMotor.setIdleMode(IdleMode.kBrake);
-    rightFrontMotor.setIdleMode(IdleMode.kBrake);
-    rightBackMotor.setIdleMode(IdleMode.kBrake);
+    leftFrontMotor.setNeutralMode(NeutralMode.Brake);
+    leftBackMotor.setNeutralMode(NeutralMode.Brake);
+    rightFrontMotor.setNeutralMode(NeutralMode.Brake);
+    rightBackMotor.setNeutralMode(NeutralMode.Brake);
   }
   public void coastDriveMotors(){
-    leftFrontMotor.setIdleMode(IdleMode.kCoast);
-    leftBackMotor.setIdleMode(IdleMode.kCoast);
-    rightFrontMotor.setIdleMode(IdleMode.kCoast);
-    rightBackMotor.setIdleMode(IdleMode.kCoast);
+    leftFrontMotor.setNeutralMode(NeutralMode.Coast);
+    leftBackMotor.setNeutralMode(NeutralMode.Coast);
+    rightFrontMotor.setNeutralMode(NeutralMode.Coast);
+    rightBackMotor.setNeutralMode(NeutralMode.Coast);
   }  
   public void toggleArcadeDriveSpeed(){
     if (arcadeDriveSpeed == "default"){
@@ -128,42 +120,57 @@ public class DriveTrain extends SubsystemBase {
   public Rotation2d getRotation2d(){
     return Rotation2d.fromDegrees(gyro.getAngle());
   }
-  // public Pose2d getPose2d(){
-  //   return m_odometry.getPoseMeters();
-  // }
+  public Pose2d getPose2d(){
+    return m_odometry.getPoseMeters();
+  }
+  
+  public double leftEncoderVelocity(){
+    return -encoderTicksToMeters(leftFrontMotor.getSelectedSensorVelocity());
+  }
+  public double rightEncoderVelocity(){
+    return -encoderTicksToMeters(rightFrontMotor.getSelectedSensorPosition()) * 10;
+  }
 
-  // public void resetEncoders(){
-  //   leftFrontMotor.setSelectedSensorPosition(0);
-  //   rightFrontMotor.setSelectedSensorPosition(0);
-  // }
-  // public void resetOdometry(Pose2d pose){
-  //   resetEncoders();
-  //   m_odometry.resetPosition(
-  //     getRotation2d(), 
-  //     encoderTicksToMeters(leftFrontMotor.getSelectedSensorPosition(), 4096, DriveTrainConstants.DRIVEGEARRATIO_LOW, DriveTrainConstants.DRIVEWHEELCIRCUMFERENCE), 
-  //     encoderTicksToMeters(rightFrontMotor.getSelectedSensorPosition(), 4096, DriveTrainConstants.DRIVEGEARRATIO_LOW, DriveTrainConstants.DRIVEWHEELCIRCUMFERENCE), 
-  //     new Pose2d());
-  // }
+  public DifferentialDriveWheelSpeeds getWheelSpeeds(){
+    return new DifferentialDriveWheelSpeeds(leftEncoderVelocity(), rightEncoderVelocity());
+  }
+
+  public double encoderTicksToMeters(double currentEncoderValue){
+    double motorRotations = currentEncoderValue / DriveTrainConstants.ENCODERFULLREV;
+    double wheelRotations = motorRotations / DriveTrainConstants.GEARRATIO_LOW;
+    double positionMeters = wheelRotations * Units.inchesToMeters(DriveTrainConstants.WHEELCIRCUMFERENCEINCHES);
+    return positionMeters;
+  }
+
+  public void resetEncoders(){
+    leftFrontMotor.setSelectedSensorPosition(0);
+    rightFrontMotor.setSelectedSensorPosition(0);
+    leftBackMotor.setSelectedSensorPosition(0);
+    rightBackMotor.setSelectedSensorPosition(0);
+  }
   public void resetGyro(){
     gyro.reset();
   }
-
-  public double encoderTicksToMeters(double currentEncoderValue, double encoderFullRev, double gearRatio, double wheelCircumferenceInInches){
-    return ((currentEncoderValue / encoderFullRev) / gearRatio) * Units.inchesToMeters(wheelCircumferenceInInches);
+  public void resetOdometry(Pose2d pose){
+    resetEncoders();
+    m_odometry.resetPosition(
+      getRotation2d(), 
+      encoderTicksToMeters(leftFrontMotor.getSelectedSensorPosition()), 
+      encoderTicksToMeters(rightFrontMotor.getSelectedSensorPosition()), 
+      new Pose2d());
   }
 
 
-  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     compressor.enableDigital(); //Runs compressor
     getRotation2d();
 
-    // m_odometry.update(
-    //   getRotation2d(), 
-    //   encoderTicksToMeters(leftFrontMotor.getSelectedSensorPosition(), 4096, DriveTrainConstants.DRIVEGEARRATIO_LOW, DriveTrainConstants.DRIVEWHEELCIRCUMFERENCE), 
-    //   encoderTicksToMeters(rightFrontMotor.getSelectedSensorPosition(), 4096, DriveTrainConstants.DRIVEGEARRATIO_LOW, DriveTrainConstants.DRIVEWHEELCIRCUMFERENCE)
-    // );
+    m_odometry.update(
+      getRotation2d(), 
+      encoderTicksToMeters(leftFrontMotor.getSelectedSensorPosition()), 
+      encoderTicksToMeters(rightFrontMotor.getSelectedSensorPosition()));
+
   }
 }
